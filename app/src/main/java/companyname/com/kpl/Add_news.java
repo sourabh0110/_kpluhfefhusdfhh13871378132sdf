fac7c75b1f4de1d35d5c7012c92a15b4b72a2ca9
Sheetal <Sheetal>
2016-11-24 18:24:41 +0530

    News

--- app/src/main/java/companyname/com/kpl/Add_news.java
+++ app/src/main/java/companyname/com/kpl/Add_news.java
@@ -1,18 +1,25 @@
 package companyname.com.kpl;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class Add_news extends AppCompatActivity {
     private ImageView iv;
     private ImageButton upload_image;
     private static final int PICK_IMAGE=100;
+    public static final int IMAGE_GALLERY_REQUEST = 20;
     Uri imageUri;
 
     @Override
@@ -25,25 +32,61 @@ public class Add_news extends AppCompatActivity {
         upload_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openGallery();
+                onImageGalleryClicked(v);
             }
         });
     }
 
-    private void openGallery()
+    private void onImageGalleryClicked(View v)
     {
-        Intent i=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(i,PICK_IMAGE);
+
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+
+        // where do we want to find the data?
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String pictureDirectoryPath = pictureDirectory.getPath();
+        // finally, get a URI representation
+        Uri data = Uri.parse(pictureDirectoryPath);
+
+        // set the data and type.  Get all image types.
+        photoPickerIntent.setDataAndType(data, "image/*");
+
+        // we will invoke this activity, and get something back from it.
+        startActivityForResult(photoPickerIntent, IMAGE_GALLERY_REQUEST);
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==RESULT_OK && resultCode==PICK_IMAGE)
-        {
-            imageUri=data.getData();
-            iv.setImageURI(imageUri);
+        if (resultCode == RESULT_OK) {
+            // if we are here, everything processed successfully.
+            if (requestCode == IMAGE_GALLERY_REQUEST) {
+                // if we are here, we are hearing back from the image gallery.
 
+                // the address of the image on the SD Card.
+                Uri imageUri = data.getData();
+
+                // declare a stream to read the image data from the SD Card.
+                InputStream inputStream;
+
+                // we are getting an input stream, based on the URI of the image.
+                try {
+                    inputStream = getContentResolver().openInputStream(imageUri);
+
+                    // get a bitmap from the stream.
+                    Bitmap image = BitmapFactory.decodeStream(inputStream);
+
+
+                    // show the image to the user
+                    iv.setImageBitmap(image);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    // show a message to the user indictating that the image is unavailable.
+                    //Toast.makeText(, "Unable to open image", Toast.LENGTH_LONG).show();
+                }
+
+            }
         }
 
     }
