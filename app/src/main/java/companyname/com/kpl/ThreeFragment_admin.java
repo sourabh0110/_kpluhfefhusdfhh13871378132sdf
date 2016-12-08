62f7828a7bf6ffdc5174b6d47c6ac4394af663cc
someone <someone@someplace.com>
2016-12-08 12:58:21 +0530

    7/12

--- app/src/main/java/companyname/com/kpl/ThreeFragment_admin.java
+++ app/src/main/java/companyname/com/kpl/ThreeFragment_admin.java
@@ -2,22 +2,46 @@ package companyname.com.kpl;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by LENOVO on 11/22/2016.
  */
 public class ThreeFragment_admin extends Fragment {
+    private Button demobutton;
 
     public ThreeFragment_admin() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        demobutton= (Button) getView().findViewById(R.id.demobutton);
+        demobutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enterNextFragment();
+            }
+
+            private void enterNextFragment() {
+                Fragment newFragment = new Demo();
+                FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack
+                transaction.replace(R.id.demo, newFragment);
+                transaction.addToBackStack(null);
+
+// Commit the transaction
+                transaction.commit();
+
+            }
+        });
     }
 
     @Override
