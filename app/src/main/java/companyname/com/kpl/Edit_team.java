47d93cec1481f919e167f2c135e65555fbfa3df6
someone <someone@someplace.com>
2016-12-09 16:24:48 +0530

    9/12

--- app/src/main/java/companyname/com/kpl/Edit_team.java
+++ app/src/main/java/companyname/com/kpl/Edit_team.java
@@ -1,11 +1,37 @@
 package companyname.com.kpl;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static android.R.attr.bitmap;
 
 public class Edit_team extends AppCompatActivity {
+    RoundImage roundImage;
+    private Bitmap bitmap;
+    private int PICK_IMAGE_REQUEST = 1;
+    FloatingActionButton uploadImage;
+    ImageView iv;
+    Spinner sel_trophy;
+    String defaultTextForSpinner = "Your deafult text here";
+    String[] arrayForSpinner = {"One", "Two", "Three"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +41,58 @@ public class Edit_team extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
         setContentView(R.layout.activity_edit_team);
+        iv= (ImageView) findViewById(R.id.iv_teamlogo);
+        Bitmap bitmap= BitmapFactory.decodeResource(getResources(),R.drawable.kpl_app_icon);
+
+        uploadImage= (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        sel_trophy= (Spinner) findViewById(R.id.spinner_trophy);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.trophies_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+
+        sel_trophy.setAdapter(adapter);
+
+
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //showFileChooser();
+            }
+
+            private void showFileChooser() {
+
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        });
     }
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                //Getting the Bitmap from Gallery
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //Setting the Bitmap to ImageView
+                iv.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
